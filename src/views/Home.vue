980c64c7330ae8a18895c7da838f0da27b98cec7
nannuflay <mohameddcharafi@gmail.com>
2022-03-06 13:54:51 +0000

    fixed and enhancement(UI) popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="mx-auto bg-base-200 w-full">
-    <div v-if="openWindow" id="popup1" class="overlay">
-      <div class="popup">
-        <h2>Welcome to Billboard 500</h2>
-        <a @click="openWindow = false" class="close" href="#">&times;</a>
-        <div class="content">
-          we use your location to determine where to suggest artists nearby you, please accept the prompt in order for us to best serve you
-        </div>
-        <button @click="getLocation()" class="button">Accept</button>
+    <!--  this will be shown only if openWindow = true -->
+    <div class="overlay" v-if="openWindow" />
+    <!-- START -- popup -->
+    <div
+      v-if="openWindow"
+      class="popup bg-white dark:bg-slate-800 md:w-3/5 w-4/5 flex justify-between p-5"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        @click="openWindow = false"
+        class="h-6 w-6 absolute top-3 right-3 hover:text-primary cursor-pointer"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18 18 6M6 6l12 12"
+        />
+      </svg>
+      <div class="w-4/6">
+        <h2 class="header text-5xl font-bold">Welcome to Billboard 500</h2>
+        <p class="text-gray-700 my-7 text-lg">
+          we use your location to determine where to suggest artists nearby you,
+          please accept the prompt in order for us to best serve you
+        </p>
+        <button @click="getLocation()" class="btn mt-10 w-48 btn-primary font-bold capitalize">
+          Accept
+        </button>
       </div>
+      <div class="w-3/6 mx-10">
+        <img class="w-full" src="public/assets/images/markers.svg" alt="" />
+      </div>
+      <div></div>
     </div>
+    <!-- END -- popup -->
 
     <div class="shadow-2xl rounded-lg w-full overflow-hidden mx-auto">
       <GoogleMap
         api-key="AIzaSyC_6hw4jl5QTB3gOgP--RC_ZGL6odyoWSo"
-        style="width: 100%; height: 80vh"
+        style="width: 100%; height: 80vh; z-index: 55"
         class="mx-auto"
         :center="center"
         :zoom="7"
@@ -37,8 +65,8 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { GoogleMap, Marker } from 'vue3-google-map';
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 
 export default defineComponent({
   components: { GoogleMap, Marker },
@@ -47,20 +75,22 @@ export default defineComponent({
       profiles: [],
       openWindow: false,
       location: null,
-      center: { lat: 40.689247, lng: -74.044502 }
+      center: { lat: 40.689247, lng: -74.044502 },
     };
   },
   created() {
     this.getProfiles();
-    let location = localStorage.getItem('location');
-    if(location) {
+    let location = localStorage.getItem("location");
+    if (location) {
       let locationObject = JSON.parse(location);
-      if(locationObject.lat && locationObject.lng) {
-        this.center = {lat: locationObject.lat, lng: locationObject.lng}
+      if (locationObject.lat && locationObject.lng) {
+        this.center = { lat: locationObject.lat, lng: locationObject.lng };
       }
     } else {
-      let user = JSON.parse(localStorage.getItem('user') || '{"logged": false, "name": null}');
-      if(!user.logged) {
+      let user = JSON.parse(
+        localStorage.getItem("user") || '{"logged": false, "name": null}'
+      );
+      if (!user.logged) {
         this.openWindow = true;
       }
     }
@@ -68,71 +98,60 @@ export default defineComponent({
   methods: {
     getProfiles() {
       let context = this;
-      let url = '/getProfiles';
+      let url = "/getProfiles";
       this.axios.get(url).then((res) => {
         context.profiles = res.data;
       });
     },
     viewProfile(index) {
       this.$router.push({
-        name: 'ArtistProfile',
+        name: "ArtistProfile",
         params: { id: this.profiles[index].profileId },
       });
     },
     getProfileIcon(index) {
-      let cat = this.profiles[index].categories[0] ? this.profiles[index].categories[0]  : 'musician';
+      let cat = this.profiles[index].categories[0]
+        ? this.profiles[index].categories[0]
+        : "musician";
       return {
-        url: './../../public/assets/images/' + this.profiles[index].categories[0] + '.png',
-        size: {width: 32, height: 50, f: 'px', b: 'px',},
-        scaledSize: {width: 30, height: 45, f: 'px', b: 'px',}
-      }
+        url:
+          "./../../public/assets/images/" +
+          this.profiles[index].categories[0] +
+          ".png",
+        size: { width: 32, height: 50, f: "px", b: "px" },
+        scaledSize: { width: 30, height: 45, f: "px", b: "px" },
+      };
     },
     getLocation() {
       this.openWindow = false;
-      navigator.geolocation.getCurrentPosition(pos => {
-        this.gettingLocation = false;
-        this.location = pos;
-        let jsonLocation = JSON.stringify({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude
-          })
-        localStorage.setItem("location", jsonLocation)
-      }, err => {
-        this.gettingLocation = false;
-        this.errorStr = err.message;
-      });
-    }
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.gettingLocation = false;
+          this.location = pos;
+          let jsonLocation = JSON.stringify({
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          });
+          localStorage.setItem("location", jsonLocation);
+        },
+        (err) => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
+    },
   },
 });
 </script>
-<style>
-h1 {
-  text-align: center;
-  font-family: Tahoma, Arial, sans-serif;
-  color: #06D85F;
-  margin: 80px 0;
-}
-
-.box {
-  width: 40%;
-  margin: 0 auto;
-  background: rgba(255,255,255,0.2);
-  padding: 35px;
-  border: 2px solid #fff;
-  border-radius: 20px/50px;
-  background-clip: padding-box;
-  text-align: center;
-}
-
-.button:hover {
-  background: #06D85F;
-}
-
+<style scoped>
 .overlay {
   position: fixed;
   opacity: 1;
   top: 0;
+  width: 100%;
+  height: 100%;
   bottom: 0;
+  z-index: 5555555544444444444;
   left: 0;
   right: 0;
   background: rgba(0, 0, 0, 0.7);
@@ -141,55 +160,12 @@ h1 {
 
 .popup {
   margin: 70px auto;
-  padding: 20px;
-  background: #fff;
+  padding: 30px;
   border-radius: 5px;
-  width: 30%;
-  position: relative;
-  transition: all 5s ease-in-out;
-}
-
-.popup h2 {
-  margin-top: 0;
-  color: #333;
-  font-family: Tahoma, Arial, sans-serif;
-}
-.popup .close {
+  z-index: 5555555555555555555555;
   position: absolute;
-  top: -10px;
-  right: 5px;
+  right: 0;
+  left: 0;
   transition: all 200ms;
-  font-size: 30px;
-  font-weight: bold;
-  text-decoration: none;
-  color: #333;
-}
-.popup .close:hover {
-  color: #06D85F;
-}
-.popup .content {
-  padding-bottom: 15px;
-  max-height: 30%;
-  overflow: auto;
-}
-
-@media screen and (max-width: 700px){
-  .box{
-    width: 70%;
-  }
-  .popup{
-    width: 70%;
-  }
-}
-
-.button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
 }
 </style>
